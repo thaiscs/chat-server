2ddae46e4e38e8eb5500b77628b7dcef7c5bd346
thaiscs <thais.advj@gmail.com>
2019-12-13 13:33:23 +0100

    updated stream

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const messageRouter = require("./messages/router");
+const messageRouterFactory = require("./messages/router");
 const bodyParser = require("body-parser");
 const parserMiddleware = bodyParser.json();
+const Sse = require("json-sse");
+const stream = new Sse();
+const messageRouter = messageRouterFactory(stream);
+const Message = require("./messages/model");
 
 const app = express();
 const port = 4000;
@@ -10,6 +14,19 @@ app.use(parserMiddleware);
 app.use(messageRouter);
 
 app.get("/", (req, resp) => {
+  stream.send("Hello from stream");
   resp.send("hello");
 });
+
+app.get("/stream", async (req, res, next) => {
+  try {
+    const messages = await Message.findAll(); // get array
+    const string = JSON.stringify(messages); // convert array into string "serialize" into series of carachters
+    stream.updateInit(string); // prepare string to be sent to the client right after they connect
+    stream.init(req, res); // connect the user to the stream
+  } catch (error) {
+    next(error);
+  }
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- messages/router.js
+++ messages/router.js
@@ -1,24 +1,31 @@
 const express = require("express");
-const { Router } = express;
-const router = new Router();
 const Message = require("./model");
+const { Router } = express;
 
-router.get("/messages", async (req, res, next) => {
-  try {
-    const messages = await Message.findAll();
-    res.send(messages);
-  } catch (error) {
-    next(error);
-  }
-});
+function factory(stream) {
+  const router = new Router();
 
-router.post("/messages", async (req, res, next) => {
-  try {
-    const message = await Message.create(req.body);
-    res.send(message);
-  } catch (error) {
-    next(error);
-  }
-});
+  router.get("/messages", async (req, res, next) => {
+    try {
+      const messages = await Message.findAll();
+      res.send(messages);
+    } catch (error) {
+      next(error);
+    }
+  });
 
-module.exports = router;
+  router.post("/messages", async (req, res, next) => {
+    try {
+      const message = await Message.create(req.body);
+      const string = JSON.stringify(message);
+      stream.send(string);
+      res.send(message);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  return router;
+}
+
+module.exports = factory;
