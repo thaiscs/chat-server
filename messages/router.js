4ab19b249fceda0d45cf9f8d7cea3c8be644df1c
thaiscs <thais.advj@gmail.com>
2019-12-13 11:22:45 +0100

    used async function

--- messages/router.js
+++ messages/router.js
@@ -3,18 +3,22 @@ const { Router } = express;
 const router = new Router();
 const Message = require("./model");
 
-router.get("/messages", (req, res, next) => {
-  Message.findAll()
-    .then(messages => {
-      res.send(messages);
-    })
-    .catch(next);
+router.get("/messages", async (req, res, next) => {
+  try {
+    const messages = await Message.findAll();
+    res.send(messages);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post("/messages", (req, res, next) => {
-  Message.create(req.body)
-    .then(message => res.json(message))
-    .catch(next);
+router.post("/messages", async (req, res, next) => {
+  try {
+    const message = await Message.create(req.body);
+    res.send(message);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
